06ab561610becd39bf5e849d1956206b7d4d9f5c
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-08-24 20:28:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,6 @@ if check_password():
     file_authentication_gs= "invoice-tool-authentication.json"
     google_sheet= "invoice-tool"
     sheet_name= "invoices"
-    url_logo = "https://i.ibb.co/12MHwBs/R.png"
 
     st.set_page_config(
         page_title='Ex-stream-ly- Cool App',
@@ -126,7 +125,6 @@ if check_password():
 
         with st.container():
             cc1,cc2 = st.columns(2)
-            cc1.image("assets/R.PNG", caption="Einnova", width=100)
             from_who = cc1.text_input("De: *",placeholder="Quien envía esta factura")
             to_who = cc1.text_input("Cobrar a: *", placeholder="Para quien es la factura")
             email = cc1.text_input("Enviar a: ", placeholder="Enviar correo (opcional)")
