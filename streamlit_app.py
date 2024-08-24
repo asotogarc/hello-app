5a5d64c95905f89487aaea83cc796c8182a71cf6
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-08-24 14:57:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-# Agregar CSS para cambiar el color de fondo
+# Agregar CSS para cambiar el color de fondo y el color del texto
 st.markdown(
     """
     <style>
     .stApp {
         background-color: #f0f0f0; /* Cambia este valor al color que desees */
     }
+    .black-text {
+        color: black; /* Cambia el color del texto a negro */
+    }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-st.write('Hello world')
+st.write('<p class="black-text">Hello world</p>', unsafe_allow_html=True)
