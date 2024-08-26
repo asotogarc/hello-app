240e5bb0f36df4fcd8353617d770a487f07eb2e5
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-08-26 18:38:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,6 +124,10 @@ if selected=="FACTURACIÓN":
         from_who = cc1.text_input("De: *",placeholder="Quien envía esta factura")
         to_who = cc1.text_input("Cobrar a: *", placeholder="Para quien es la factura")
         email = cc1.text_input("Enviar a: ", placeholder="Enviar correo (opcional)")
+        num_invoice = cc2.text_input("#", placeholder='Numero de factura')
+        date_invoice = cc2.date_input("Fecha *")
+        due_date = cc2.date_input("Fecha de vencimiento")
+
 
         if email:
             validation = validate_email(email)
